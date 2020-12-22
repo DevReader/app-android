0d4d80465758017cc23248bd5df9317719fadf3c
rx1310 <rx1310@inbox.ru>
2020-12-22 21:30:24 +0300

    Добавил WebView

--- app/src/main/java/ru/devreader/app/activity/MainActivity.java
+++ app/src/main/java/ru/devreader/app/activity/MainActivity.java
@@ -35,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	final String loadUrl = "https://" + "devreader.github.io" + "/";
 	
 	Toolbar mToolbar;
+	WebView mWebView;
+	
+	boolean dbg_javaScript = true, dbg_appCache = true;
 	
 	SharedPreferences mSharedPrefs;
 	SharedPreferences.Editor mSharedPrefsEditor;
@@ -48,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		mSharedPrefsEditor = mSharedPrefs.edit();
 
 		initToolbar();
+		initWebView();
 		
 	}
 	
@@ -73,6 +77,100 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 			default: break;
 
 		}
+
+	}
+	
+	void initWebView() {
+
+		AppUtils.Log(this, "d", "Init WebView");
+
+		mWebView = findViewById(R.id.el_webView);
+
+		WebSettings WebViewSettings = mWebView.getSettings();
+		WebViewSettings.setDefaultTextEncodingName("utf-8");
+
+		if (dbg_javaScript) {
+			AppUtils.Log(this, "d", "dbg.javaScript: " + dbg_javaScript);
+			mWebView.getSettings().setJavaScriptEnabled(true);
+			mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+		}
+
+		if (dbg_appCache) {
+			AppUtils.Log(this, "d", "dbg.appCache: " + dbg_appCache);
+			mWebView.getSettings().setAppCacheEnabled(true);
+			mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+		}
+
+		mWebView.setBackgroundColor(Color.parseColor("#121212"));
+		mWebView.getSettings().setDomStorageEnabled(true);
+		mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+		mWebView.setWebViewClient(new WebViewClient() {
+
+				public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+
+					String urlPrefix = loadUrl;
+
+					if (url != null && url.startsWith(urlPrefix)){
+						return false;
+					} else {
+						webView.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+						return true;
+					}
+
+				}
+
+				@SuppressWarnings("deprecation")
+				@Override
+				public void onReceivedError(WebView webView, int errCode, String errDesc, String failingUrl) {
+
+					String log = "code: " + errCode + "\ndesc: " + errDesc + "\nurl: " + failingUrl;
+					AppUtils.Log(MainActivity.this, "e", log);
+
+				}
+
+			});
+
+		mWebView.setWebChromeClient(new WebChromeClient() {
+
+				public void onProgressChanged(WebView webView, int nProgress) {
+
+					AppUtils.Log(MainActivity.this, "i", "onProgressChanged");
+
+					if (nProgress < 100) {
+
+						AppUtils.Log(MainActivity.this, "i", "nProgress < 100");
+						mWebView.setVisibility(View.GONE);
+
+					} else if (nProgress == 100) {
+
+						AppUtils.Log(MainActivity.this, "i", "nProgress == 100");
+
+						mWebView.setVisibility(View.VISIBLE);
+
+					} else {
+
+						AppUtils.Log(MainActivity.this, "i", "nProgress / else");
+						mWebView.setVisibility(View.GONE);
+
+					}
+
+				}
+
+
+				@Override
+				public void onReceivedTitle(WebView webView, String pageTitle) {
+					super.onReceivedTitle(webView, pageTitle);
+					if (!TextUtils.isEmpty(pageTitle)) {
+						mToolbar.setSubtitle(pageTitle);
+					}
+				}
+
+			});
+
+		//mWebView.loadUrl("file:///android_asset/" + "index.html");
+		mWebView.loadUrl(loadUrl);
+		AppUtils.Log(MainActivity.this, "i", "WebView load url: " + loadUrl);
 
 	}
 	
