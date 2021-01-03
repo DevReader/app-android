2e44d89b7c4f7f76fab22acee06b8111971a480a
rx1310 <rx1310@inbox.ru>
2021-01-03 20:47:48 +0300

    Исправил ссылки на JSON-файлы в приложении

--- app/src/main/java/ru/devreader/app/Constants.java
+++ app/src/main/java/ru/devreader/app/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 	
 	public static class OTA {
 		
-		public static final String CHANNEL_RELEASE = "https://raw.githubusercontent.com/rx1310/ota-test/master/ota_stable.json";
-		public static final String CHANNEL_BETA = "https://raw.githubusercontent.com/rx1310/ota-test/master/ota_beta.json";
+		public static final String CHANNEL_RELEASE = "https://raw.githubusercontent.com/devreader/app-android/master/ota/release.json";
+		public static final String CHANNEL_BETA = "https://raw.githubusercontent.com/devreader/app-android/master/ota/beta.json";
 		public static final String URL_APK = "apkUrl";
 		public static final String URL_CHANGELOG = "changelogUrl";
 		public static final String VERSION_NAME = "versionName";
